13dac1d421982b6f24f2b8e58521cc49419535c5
prativam <92173580+prativam@users.noreply.github.com>
2022-07-06 10:24:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const http = require('http');
+const port =  5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+http.createServer(function(req,res) {
+  let url = req.url;
+  res.writeHead(200, {
+    'Content-Type':'text/html'
+  });
+
+  res.write('<h1>Hello world! => version v3(Azure Devops</h1>');
+  res.end();
+}).listen(port, function(){
+  console.log('Server started at port 5000');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
